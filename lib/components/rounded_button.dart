eea82810cc89c4fc04f1707d7f49122e6b07efe5
José Naves <josenaves@gmail.com>
2019-09-01 19:39:33 -0300

    refactor: Refactor code to get small codebase

---
+++ lib/components/rounded_button.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class RoundedButton extends StatelessWidget {
+  final Color color;
+  final String title;
+  final Function onPressed;
+
+  RoundedButton({this.color, this.title, this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 16.0),
+      child: Material(
+        elevation: 5.0,
+        color: color,
+        borderRadius: BorderRadius.circular(30.0),
+        child: MaterialButton(
+          onPressed: onPressed,
+          minWidth: 200.0,
+          height: 42.0,
+          child: Text(
+            title,
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/constants.dart
+++ lib/constants.dart
@@ -17,3 +17,19 @@ const kMessageContainerDecoration = BoxDecoration(
     top: BorderSide(color: Colors.lightBlueAccent, width: 2.0),
   ),
 );
+
+const kTextFieldDecoration = InputDecoration(
+  hintText: 'Enter a value',
+  contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+  ),
+  enabledBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.blueAccent, width: 1.0),
+    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+  ),
+  focusedBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.blueAccent, width: 2.0),
+    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+  ),
+);
