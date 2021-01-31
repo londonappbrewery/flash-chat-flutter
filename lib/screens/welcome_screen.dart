d224a82ddd1431d2739db3d12902605e1e612dc1
PrestoSole <yalather70@pm.me>
2021-01-31 14:38:25 +0300

    Add AnimationController

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -11,12 +11,33 @@ class WelcomeScreen extends StatefulWidget {
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
-class _WelcomeScreenState extends State<WelcomeScreen> {
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with SingleTickerProviderStateMixin {
+  AnimationController controller;
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this,
+    );
+    controller.forward();
+    controller.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     final mq = MediaQuery.of(context);
+
     return Scaffold(
-      //backgroundColor: Color(0xff8c52ff),
       backgroundColor: const Color(0xFFCE265A),
       body: Padding(
         padding: const EdgeInsets.all(36.0),
