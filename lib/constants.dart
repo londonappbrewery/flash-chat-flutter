2ae25081c4e07a4184f09e30e967f1fbd6d1cc5d
Osama <syed.m.osama.new@gmail.com>
2021-11-01 15:46:36 +0530

    completed the flash chat app.

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,24 @@
 import 'package:flutter/material.dart';
 
+const kButtonDecoration = InputDecoration(
+  hintText: 'Enter your info',
+  hintStyle: TextStyle(
+    color: Colors.lightBlueAccent,
+  ),
+  contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+  ),
+  enabledBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.lightBlueAccent, width: 1.0),
+    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+  ),
+  focusedBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.lightBlueAccent, width: 2.0),
+    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+  ),
+);
+
 const kSendButtonTextStyle = TextStyle(
   color: Colors.lightBlueAccent,
   fontWeight: FontWeight.bold,
@@ -14,6 +33,6 @@ const kMessageTextFieldDecoration = InputDecoration(
 
 const kMessageContainerDecoration = BoxDecoration(
   border: Border(
-    top: BorderSide(color: Colors.lightBlueAccent, width: 2.0),
+    top: BorderSide(color: Colors.blue, width: 2.0),
   ),
 );
--- lib/main.dart
+++ lib/main.dart
@@ -3,19 +3,34 @@ import 'package:flash_chat/screens/welcome_screen.dart';
 import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flash_chat/screens/chat_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() => runApp(FlashChat());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(FlashChat());
+}
 
 class FlashChat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark().copyWith(
-        textTheme: TextTheme(
-          body1: TextStyle(color: Colors.black54),
-        ),
-      ),
-      home: WelcomeScreen(),
+      theme: ThemeData.light().copyWith(),
+      initialRoute: '/',
+      routes: {
+        WelcomeScreen.id: (context) {
+          return WelcomeScreen();
+        },
+        LoginScreen.id: (context) {
+          return LoginScreen();
+        },
+        RegistrationScreen.id: (context) {
+          return RegistrationScreen();
+        },
+        ChatScreen.id: (context) {
+          return ChatScreen();
+        }
+      },
     );
   }
 }
