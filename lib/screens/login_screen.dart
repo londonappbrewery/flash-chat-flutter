825fc2a6d44b28b4abd41912ace1023fc773722c
nananyanibaquartey <nyaniba.quartey@gmail.com>
2022-01-15 12:58:41 +0000

    Create id variable

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
+  static String id = 'login_screen';
+
   @override
   _LoginScreenState createState() => _LoginScreenState();
 }
