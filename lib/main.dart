b855fd4e8b12a0c6352b48943d671fc59876d179
nananyanibaquartey <nyaniba.quartey@gmail.com>
2022-01-15 17:15:42 +0000

    Add named routes

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,13 @@ class FlashChat extends StatelessWidget {
           bodyText1: TextStyle(color: Colors.black54),
         ),
       ),
-      home: WelcomeScreen(),
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+        ChatScreen.id: (context) => ChatScreen(),
+      },
     );
   }
 }
