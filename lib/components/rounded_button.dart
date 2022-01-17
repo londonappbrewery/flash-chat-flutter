034d085f3da387f84196242c8bcc992c03c4c435
nananyanibaquartey <nyaniba.quartey@gmail.com>
2022-01-17 19:28:38 +0000

    New file for custom button

---
+++ lib/components/rounded_button.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class RoundedButton extends StatelessWidget {
+  const RoundedButton({this.colour, this.title, @required this.onPressed});
+
+  final Color colour;
+  final String title;
+  final Function onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 16.0),
+      child: Material(
+        elevation: 5.0,
+        color: colour,
+        borderRadius: BorderRadius.circular(30.0),
+        child: MaterialButton(
+          onPressed: onPressed,
+          minWidth: 200.0,
+          height: 42.0,
+          child: Text(title),
+        ),
+      ),
+    );
+  }
+}
