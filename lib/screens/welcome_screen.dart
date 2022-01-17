669c34698ad4b8904932412e0167de5fd0609dbb
nananyanibaquartey <nyaniba.quartey@gmail.com>
2022-01-17 19:30:06 +0000

    Move custom RoundedButton class into a new file

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -2,6 +2,7 @@ import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flash_chat/components/rounded_button.dart';
 
 class WelcomeScreen extends StatefulWidget {
   static const String id = 'welcome_screen';
@@ -91,32 +92,6 @@ class _WelcomeScreenState extends State<WelcomeScreen>
               },
             )
           ],
-        ),
-      ),
-    );
-  }
-}
-
-class RoundedButton extends StatelessWidget {
-  const RoundedButton({this.colour, this.title, @required this.onPressed});
-
-  final Color colour;
-  final String title;
-  final Function onPressed;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(vertical: 16.0),
-      child: Material(
-        elevation: 5.0,
-        color: colour,
-        borderRadius: BorderRadius.circular(30.0),
-        child: MaterialButton(
-          onPressed: onPressed,
-          minWidth: 200.0,
-          height: 42.0,
-          child: Text(title),
         ),
       ),
     );
