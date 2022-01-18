891eecdf093cb9be17347013877ab149cd86d5ab
nananyanibaquartey <nyaniba.quartey@gmail.com>
2022-01-18 20:24:58 +0000

    Initialize app to use Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,13 @@ import 'package:flash_chat/screens/welcome_screen.dart';
 import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flash_chat/screens/chat_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() => runApp(FlashChat());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(FlashChat());
+}
 
 class FlashChat extends StatelessWidget {
   @override
